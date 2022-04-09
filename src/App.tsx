688f19cf8e7e1004379e60f1827362afc386f359
rnuravtsev <muravtsev21@gmail.com>
2022-04-09 07:21:41 +0300

    base blocks

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from "./blocks/header/Header";
+import Footer from "./blocks/footer/Footer";
+import Main from "./blocks/main/Main";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header/>
+            <Main/>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/blocks/main/Main.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Main = () => {
+    return (
+        <main className="main">
+            <h1>Прогноз погоды</h1>
+        </main>
+    );
+};
+
+export default Main;
